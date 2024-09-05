770a20b0c846f09cfbc30a7adab01a0991679321
shubhams <shubhams@legal.regn.net>
2024-09-05 18:33:08 -0400

    add

---
+++ 01-Udemy-EKS-Terraform/11-AWS-EBSCSIDriver-AddOn/ebs-csi-iam-role.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "ebs_csi_iam_role" {
+  name = "${local.name}-ebs-csi-iam-role"
+
+  # Terraform's "jsonencode" function converts a Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn}"
+        }
+        Condition = {
+          StringEquals = {
+            "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn}:sub" : "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+
+      },
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    tag-key = "${local.name}-ebs-csi-iam-role"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
+  policy_arn = aws_iam_policy.ebs_csi_iam_policy.arn
+  role       = aws_iam_role.ebs_csi_iam_role.name
+}
+
+output "ebs_csi_iam_role_arn" {
+  description = "EBS CSI IAM Role ARN"
+  value       = aws_iam_role.ebs_csi_iam_role.arn
+}
