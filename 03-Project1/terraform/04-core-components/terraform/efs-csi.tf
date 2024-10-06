2ea701c47190c40ba35811dd1207d400d22d850e
shubhams <shubhams@legal.regn.net>
2024-10-06 17:38:33 -0400

    add EFS CSI Driver with persistant storage

--- 03-Project1/terraform/04-core-components/terraform/efs-csi.tf
+++ 03-Project1/terraform/04-core-components/terraform/efs-csi.tf
@@ -40,3 +40,44 @@ resource "aws_iam_role_policy_attachment" "efs_csi_iam_role_policy_attach" {
   policy_arn = aws_iam_policy.efs_csi_iam_policy.arn 
   role       = aws_iam_role.efs_csi_iam_role.name
 }
+
+# Resource: Security Group - Allow Inbound NFS Traffic from EKS VPC CIDR to EFS File System
+resource "aws_security_group" "efs_allow_access" {
+  name        = "${local.resource_prefix}-efs-allow-nfs-from-eks-vpc"
+  description = "Allow Inbound NFS Traffic from EKS VPC CIDR"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description      = "Allow Inbound NFS Traffic from EKS VPC CIDR to EFS File System"
+    from_port        = 2049
+    to_port          = 2049
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
+
+
+# Resource: EFS File System
+resource "aws_efs_file_system" "efs_file_system" {
+  creation_token = "efs-demo"
+  tags = merge(local.common_tags, {
+    Name = "efs-demo"
+  })
+}
+
+# Resource: EFS Mount Target
+resource "aws_efs_mount_target" "efs_mount_target" { # mount target is needed so that the pods can access the PVC
+  count = 2
+  file_system_id = aws_efs_file_system.efs_file_system.id
+  subnet_id      = data.terraform_remote_state.vpc.outputs.private_subnets[count.index]
+  security_groups = [ aws_security_group.efs_allow_access.id ]
+}
--- 03-Project1/terraform/04-core-components/terraform/remote-state-storage.tf
+++ 03-Project1/terraform/04-core-components/terraform/remote-state-storage.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state" "eks_cluster" {
     region = var.aws_region
   }
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "4054-shubhams-dev"
+    key    = "dev/eks/terraform.tfstate"
+    region = var.aws_region
+  }
+}
