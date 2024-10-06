597c41d2ef89a71db52d4519a04b58c9ba247918
shubhams <shubhams@legal.regn.net>
2024-10-06 18:49:49 -0400

    add helm chart for AWS EBS CSI Driver

--- 03-Project1/terraform/04-core-components/terraform/data-sources.tf
+++ 03-Project1/terraform/04-core-components/terraform/data-sources.tf
@@ -22,3 +22,13 @@ data "http" "efs_csi_iam_policy" {
     Accept = "application/json"
   }
 }
+
+# Datasource: EBS CSI IAM Policy get from EBS GIT Repo (latest)
+data "http" "ebs_csi_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
